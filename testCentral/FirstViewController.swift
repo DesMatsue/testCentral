78115d35ccdcf2b7d06b7b9fe8a91beec5ebbb6b
DesMatsue <takakura-y@macd1189.local>
2017-09-14 18:46:02 -0700

    Initial Commit

---
+++ testCentral/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  testCentral
+//
+//  Created by Takakura 高倉 優介 on 2017/09/14.
+//  Copyright © 2017年 DesMatsue. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
